24ab3f3fe67c2516ae12e19a07775d8d36a4ca34
El Bir Ahmed <32835046+elbirahmed@users.noreply.github.com>
2023-05-23 23:53:56 +0200

    Update streamlit_app.py
    add one item and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title("My Parents New Heathy Diner") 
-streamlit.header("Breakfast Menu")
-streamlit.text('Omega 3 & Blueberry OatMeal') 
-streamlit.text('Kale, Spinach & Rocket Smoothie') 
-streamlit.text('Hard-Boiled Free-Range Egg') 
+streamlit.title("My Parents New Heathy Diner")    
+streamlit.header("Breakfast Favorites")
+streamlit.text('🥣 Omega 3 & Blueberry OatMeal') 
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie') 
+streamlit.text('🐔 Hard-Boiled Free-Range Egg') 
+streamlit.text('🥑🍞 Avpcado Toast') 
