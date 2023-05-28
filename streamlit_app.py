06eb54607f0be2368e62168820f968ebf2a20a4b
El Bir Ahmed <32835046+elbirahmed@users.noreply.github.com>
2023-05-28 13:41:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 streamlit.text("Hello from Snowflake:")
 
